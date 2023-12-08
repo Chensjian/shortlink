6862ba141f7078f61e714b2e7986c354cd0bc255
chenchenchen <2811908526@qq.com>
2023-12-08 18:28:58 +0800

    常量定义

---
+++ admin/src/main/java/com/chen/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,8 @@
+package com.chen.shortlink.admin.common.constant;
+
+/**
+ * redis缓存常量
+ */
+public class RedisCacheConstant {
+    public static final String LOCK_USER_REGISTER_KEY="short-link:lock_user_register:";
+}
